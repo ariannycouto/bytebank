8390a0c5c18faadaee3b7f704a079021ffdf2558
16396461757 <ariannycouto682@gmail.com>
2021-09-19 19:52:21 -0300

    primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
+
+import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localept from '@angular/common/locales/pt'
+import { AppRountingModule } from './app-rounting.module';
+
+registerLocaleData(localept, 'pt');
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent,
+    ExtratoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRountingModule
+  ],
+
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    },
+
+  ],
+
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/model/transferencia.model.ts
@@ -0,0 +1,12 @@
+  export interface Transferencia {
+      id?: string;
+      valor: number;
+      destino: number | string;
+      data?: Date;
+  }
+
+  export interface RootObject {
+      transferencias: Transferencia[];
+  }
+
+
